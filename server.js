d22bdf60a9dcb504c4d8be4c6cdfd0818bd3c063
unknown <hima.ratikanti@gmail.com>
2017-06-04 11:27:13 +0530

    imp

--- server.js
+++ server.js
@@ -8,37 +8,124 @@ var user=require("./user.js");
 
 var bodyparser=require("body-parser");
 
-// app.use(bodyparser.json());
-// app.use(bodyparser.urlencoded({extended:true}))
+ app.use(bodyparser.json());
+ app.use(bodyparser.urlencoded({extended:true}))
 
 mongoose.connect("mongodb://localhost/techera",function(){
 	console.log("sucessfully connected to mongodb")
 })
 
-router.get("/",function(request,response){
-         reponse.json({name:"asha"})
-})
-router.get("/users",function(request,response){
-         user.getUser(function(err,data){
-         	if(err){
-         		throw err;
-         	}
-         	console.log(data)
-         	response.json(data)
-
-         })
-})
-
-// router.post("/customer",function(request,response){
-// 	var customerobj=request.body;
-//          customer.createCustomer(customerobj,function(err,data){
+// router.get("/",function(request,response){
+  //        reponse.json({name:"asha"})
+// })
+// router.get("/users",function(request,response){
+//          user.getUser(function(err,data){
 //          	if(err){
 //          		throw err;
 //          	}
+//          	console.log(data)
 //          	response.json(data)
 
 //          })
 // })
+
+//  router.post("/customer",function(request,response){
+//  	var customerobj=request.body;
+//           customer.createCustomer(customerobj,function(err,data){
+//          	if(err){
+//           		throw err;
+//           	}
+//          	response.json(data)
+
+//           })
+// })
+
+
+// router.put("/user/:id",function(request,response){
+//       var userid=request.params.id; //parameter in url
+//       var customerobj=request.body;
+//       console.log(customerobj);
+//      user.editCustomer(userid,customerobj,function(err,data){
+//          if(err){
+//             throw err;
+//          }
+//          response.json(data)
+//       })
+// })
+
+
+// router.delete("/user/:id",function(request,response){
+//       var userid=request.params.id; //parameter in url
+//       var customerobj=request.body;
+//       console.log(customerobj);
+//      user.deleteuser(userid,customerobj,function(err,data){
+//          if(err){
+//             throw err;
+//          }
+//           response.json(data)
+//       })
+// })
+
+ // router.get("/",function(request,response){
+ //        reponse.json({name:"asha"})
+ // })
+//  router.get("/users/:id",function(request,response){
+//    var userid=request.params.id;
+//          user.getElementById(userid,function(err,data){
+//             if(err){
+//                 throw err;
+//             }
+//              console.log(data)
+//            response.json(data)
+
+//          })
+// })
+
+
+// router.get("/users/:name",function(request,response){
+//    var userid=request.params.name;
+//          user.getElementById(userid,function(err,data){
+//             if(err){
+//                 throw err;
+//             }
+//              console.log(data)
+//            response.json(data)
+
+//          })
+// })
+
+
+router.put("/user/:id",function(request,response){  //first we have to run get then goto put then send data
+      var userid=request.params.id; //parameter in url
+      var customerobj=request.body;//from database
+     
+     user.getElementById(userid,function(err,data){
+            if(err){
+                throw err;
+            }
+              console.log(data)
+           
+           
+           response.json(data)
+
+
+      
+     console.log(data);
+     console.log(customerobj)
+     var bodyObj={              //to avoid getting null into database
+      name:customerobj.name || data.name,//data from database
+      email:customerobj.email || data.email,
+      mail:customerobj.mail || data.mail,
+      mobile:customerobj.mobile || data.mobile,
+     }
+      user.editCustomer(userid,bodyObj,function(err,data){
+         if(err){
+             throw err;
+          }
+          response.json(data)
+       })
+       });
+ })
 
 
 
--- user.js
+++ user.js
@@ -8,19 +8,56 @@ var userSchema=mongoose.Schema({
 		type:String,
 		required:true
 	},
+	mail:{
+		type:String,
+		required:true
+	},
+
 	mobile:{ //should match to db property
 		type:String,
 		required:true
 	}
 });
 
-var User=module.exports=mongoose.model("user",userSchema,"user")
+ var User=module.exports=mongoose.model("user",userSchema,"user")
 
-module.exports.getUser=function(callback){
-	return User.find(callback)
-	//console.log(data)
-}
+//  module.exports.getUser=function(callback){
+// 	return User.find(callback)
+//  	//console.log(data)
+//  }
 
 // module.exports.createCustomer=function(customerobj,callback){
 // 	return User.create(customerobj,callback)
-// }
+//  }
+
+  module.exports.editCustomer=function(userid,customerObj,callback){
+ 	console.log(customerObj);
+  	return User.update({_id:userid},{$set:{
+
+ 		                               name:customerObj.name,
+ 		                               mail:customerObj.mail,
+ 		                               mobile:customerObj.mobile,
+  		                               email:customerObj.email
+
+	                                    }},callback)
+ 	 
+  }
+
+
+ //  module.exports.deleteuser=function(userid,customerObj,callback){
+	// console.log(customerObj);
+ // 	return User.remove({_id:userid},callback)
+ 	 
+ // }
+
+   module.exports.getElementById=function(userid,callback){
+	
+ 	return User.findById({_id:userid},callback)
+ 	 
+  }
+
+ // module.exports.getElementById=function(name,callback){
+	
+ // 	return User.findOne({name:name},callback)
+ 	 
+ // }
